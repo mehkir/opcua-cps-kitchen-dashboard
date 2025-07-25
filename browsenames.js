21ae05223d99f2a24329292428ddcb7916b75a36
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-25 20:02:35 +0200

    Add browsenames

---
+++ browsenames.js
@@ -0,0 +1,43 @@
+/* ROBOT */
+const Robot = {
+    // object type node
+    TYPE : "RobotType",
+    // method nodes
+    RECEIVE_TASK : "ReceiveTask",
+    HANDOVER_FINISHED_ORDER : "HandoverFinishedOrder",
+    // information nodes
+    AGENT_TYPE : "AgentType",
+    RECIPE_ID : "RecipeId",
+    DISH_NAME : "DishName",
+    ACTION_NAME : "ActionName",
+    INGREDIENTS : "Ingredients",
+    OVERALL_TIME : "OverallTime",
+    CURRENT_TOOL : "CurrentTool",
+    LAST_EQUIPPED_TOOL : "LastEquippedTool",
+    CAPABILITIES : "Capabilities"
+};
+
+/* CONVEYOR */
+const Conveyor = {
+    // object type node
+    TYPE : "ConveyorType",
+    PLATE_TYPE : "PlateType",
+    // plate information nodes
+    PLATE_ID : "Id",
+    PLATE_POSITION : "Position",
+    PLATE_RECIPE_ID : "RecipeId",
+    PLATE_OCCUPIED : "Occupied",
+    // method nodes
+    FINISHED_ORDER_NOTIFICATION : "FinishedOrderNotification"
+};
+
+/* CONTROLLER */
+const Controller = {
+    // object type node
+    TYPE : "ControllerType",
+    // method nodes
+    REGISTER_ROBOT : "RegisterRobot",
+    CHOOSE_NEXT_ROBOT : "ChooseNextRobot",
+    PLACE_RANDOM_ORDER : "PlaceRandomOrder"
+};
+module.exports = { Robot, Conveyor, Controller };
