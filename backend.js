1b7726bae788443ca99f68fda4eb10701a6715e1
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-27 19:32:10 +0200

    Subscribe to all attributes of robots

--- backend.js
+++ backend.js
@@ -10,12 +10,12 @@ async function read_attribute_value(_server_url, _node_id) {
     const client = OPCUAClient.create({});
     await client.connect(_server_url);
     const session = await client.createSession();
-    const dataValue = await session.read({
+    const data_value = await session.read({
         nodeId: _node_id,
     });
     await session.close();
     await client.disconnect();
-    return dataValue.value.value;
+    return data_value.value.value;
 }
 
 const robots = new Map();
@@ -23,7 +23,7 @@ const conveyor = {};
 conveyor.plates = new Map();
 const controller = {};
 controller.methods = {};
-(async () => {
+await (async () => {
     for (const server of servers) {
         if (server.applicationType !== ApplicationType.Server) {
             console.log(`Skipping non-server application: ${server.applicationUri}`);
@@ -33,14 +33,15 @@ controller.methods = {};
         if ((instance_id = await opcua_browser_instance.browse_instance(server.discoveryUrl, Robot.TYPE)) !== NodeId.nullNodeId) {
             console.log(`Robot type found on server: ${server.discoveryUrl}`);
             const browse_attributes_result = await opcua_browser_instance.browse_attributes(server.discoveryUrl, instance_id);
-            robot_server = {};
+            const robot_server = {};
             robot_server.attributes = {};
             for (const attr of browse_attributes_result.references) {
                 console.log(`Robot attribute: ${attr.browseName.name} (${attr.nodeId.toString()})`);
-                robot_server.attributes[attr.browseName.name] = attr.nodeId;
                 if (attr.browseName.name === Robot.POSITION) {
                     robot_server.position = await read_attribute_value(server.discoveryUrl, attr.nodeId);
+                    continue;
                 }
+                robot_server.attributes[attr.browseName.name] = attr.nodeId;
             }
             robot_server.url = server.discoveryUrl;
             robots.set(robot_server.position, robot_server);
@@ -50,7 +51,7 @@ controller.methods = {};
             console.log(`Conveyor type found on server: ${server.discoveryUrl}`);
             const browse_objects_result = await opcua_browser_instance.browse_objects(server.discoveryUrl, instance_id, resolveNodeId("HasComponent"));
             for (const obj of browse_objects_result.references) {
-                plate_attributes = {};
+                const plate_attributes = {};
                 console.log(`Plate object: ${obj.browseName.name} (${obj.nodeId.toString()})`);
                 const browse_attributes_result = await opcua_browser_instance.browse_attributes(server.discoveryUrl, obj.nodeId);
                 for (const attr of browse_attributes_result.references) {
@@ -75,4 +76,22 @@ controller.methods = {};
             controller.url = server.discoveryUrl;
         }
     }
+})();
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+const opcua_subscriber = require('./opcua-subscription.js');
+(async () => {
+    robots.forEach(async (robot, pos) => {
+        const opcua = new opcua_subscriber(wss, robot.url);
+        await opcua.create_session();
+        await opcua.create_subscription();
+        const robot_monitor = {};
+        robot_monitor.position = pos;
+        for (const [browse_name, attribute_id] of Object.entries(robot.attributes)) {
+            robot_monitor.attribute_name = browse_name;
+            console.log(`Subscribing to robot at position ${pos}`);
+            await opcua.subscribe(attribute_id, robot_monitor);
+        }
+    });
 })();
