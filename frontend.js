693ef59feca8a0ead02b65e96f3e7feb35986cdc
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-07-28 22:05:25 +0200

    Refactor and add setup environment

--- frontend.js
+++ frontend.js
@@ -1,3 +1,46 @@
+/* ROBOT */
+const Robot = {
+    // object type node
+    TYPE : "RobotType",
+    // method nodes
+    RECEIVE_TASK : "ReceiveTask",
+    HANDOVER_FINISHED_ORDER : "HandoverFinishedOrder",
+    // information nodes
+    POSITION : "Position",
+    RECIPE_ID : "RecipeId",
+    DISH_NAME : "DishName",
+    ACTION_NAME : "ActionName",
+    INGREDIENTS : "Ingredients",
+    OVERALL_TIME : "OverallTime",
+    CURRENT_TOOL : "CurrentTool",
+    LAST_EQUIPPED_TOOL : "LastEquippedTool",
+    CAPABILITIES : "Capabilities"
+};
+
+/* CONVEYOR */
+const Conveyor = {
+    // object type node
+    TYPE : "ConveyorType",
+    PLATE_TYPE : "PlateType",
+    // plate information nodes
+    PLATE_ID : "Id",
+    PLATE_POSITION : "Position",
+    PLATE_RECIPE_ID : "RecipeId",
+    PLATE_OCCUPIED : "Occupied",
+    // method nodes
+    FINISHED_ORDER_NOTIFICATION : "FinishedOrderNotification"
+};
+
+/* CONTROLLER */
+const Controller = {
+    // object type node
+    TYPE : "ControllerType",
+    // method nodes
+    REGISTER_ROBOT : "RegisterRobot",
+    CHOOSE_NEXT_ROBOT : "ChooseNextRobot",
+    PLACE_RANDOM_ORDER : "PlaceRandomOrder"
+};
+
 const gauges = {};
 
 function create_robot_info_element(pos) {
@@ -7,11 +50,11 @@ function create_robot_info_element(pos) {
 
   p.innerHTML = `
     <span style="display: block; text-align: center;">Pos ${pos}</span><br>
-    Recipe ID: <span id="recipe-${pos}">None</span> <br>
-    Dish name: <span id="dish-${pos}">None</span> <br>
-    Current tool: <span id="tool-${pos}">None</span> <br>
-    Action: <span id="action-${pos}">None</span> <br>
-    Ingredients: <span id="ingredients-${pos}">None</span>
+    Recipe ID: <span id="${Robot.RECIPE_ID}-${pos}">None</span> <br>
+    Dish name: <span id="${Robot.DISH_NAME}-${pos}">None</span> <br>
+    Current tool: <span id="${Robot.CURRENT_TOOL}-${pos}">None</span> <br>
+    Action: <span id="${Robot.ACTION_NAME}-${pos}">None</span> <br>
+    Ingredients: <span id="${Robot.INGREDIENTS}-${pos}">None</span>
   `;
 
   return p;
@@ -26,30 +69,30 @@ function update_robot_info_element(pos, data) {
         }
     };
 
-    set_if_exists('recipe', data.recipe);
-    set_if_exists('dish', data.dish);
-    set_if_exists('tool', data.tool);
-    set_if_exists('action', data.action);
-    set_if_exists('ingredients', data.ingredients);
+    set_if_exists(Robot.RECIPE_ID, data.recipe);
+    set_if_exists(Robot.DISH_NAME, data.dish);
+    set_if_exists(Robot.CURRENT_TOOL, data.tool);
+    set_if_exists(Robot.ACTION_NAME, data.action);
+    set_if_exists(Robot.INGREDIENTS, data.ingredients);
 }
 
 
-function createGauge(pos) {
+function create_gauge(pos) {
     const div = document.createElement('div');
     div.id = `gauge-${pos}`;
     return div;
 }
 
-function create_plate_info_element(pos) {
+function create_plate_info_element(pos, label) {
     const p = document.createElement('p');
     p.className = 'open-sans-myfont';
     p.id = `plate-${pos}`;
 
     p.innerHTML = `
-        <span style="display: block; text-align: center;">Pos ${pos}</span><br>
-        ID: <span id="plate-id-${pos}">None</span> <br>
-        Recipe ID: <span id="plate-recipe-${pos}">None</span> <br>
-        Occupied: <span id="plate-occupied-${pos}">None</span>
+        <span style="display: block; text-align: center;">${label}</span><br>
+        ID: <span id="${Conveyor.PLATE_ID}-${pos}">None</span> <br>
+        Recipe ID: <span id="${Conveyor.PLATE_RECIPE_ID}-${pos}">None</span> <br>
+        Occupied: <span id="${Conveyor.PLATE_OCCUPIED}-${pos}">None</span>
     `;
 
     return p;
@@ -64,40 +107,13 @@ function update_plate_info_element(pos, data) {
         }
     };
 
-    set_if_exists('id', data.id);
-    set_if_exists('recipe', data.recipe);
-    set_if_exists('occupied', data.occupied);
+    set_if_exists(Conveyor.PLATE_ID, data.id);
+    set_if_exists(Conveyor.PLATE_RECIPE_ID, data.recipe);
+    set_if_exists(Conveyor.PLATE_OCCUPIED, data.occupied);
 }
 
 function handle_received_data(pos, value) {
-    const robot_id = `robot-${pos}`;
-    if (!document.getElementById(robot_id)) {
-        const new_robot = create_robot_info_element(pos);
-        const container = document.getElementById("robots")
-        container.appendChild(new_robot);
-    }
-    const gauge_id = `gauge-${pos}`;
-    if (!document.getElementById(gauge_id)) {
-        const new_gauge = createGauge(pos);
-        const container = document.getElementById("gauges")
-        container.appendChild(new_gauge);
-
-        gauges[pos] = new JustGage({
-            id: `gauge-${pos}`,
-            value: 0,
-            min: 0,
-            max: 100,
-            label: "units"
-        });
-    } else {
-        gauges[pos].refresh(value);
-    }
-    const plate_id = `plate-${pos}`;
-    if (!document.getElementById(plate_id)) {
-        const new_plate = create_plate_info_element(pos);
-        const container = document.getElementById("plates")
-        container.appendChild(new_plate);
-    }
+    // gauges[pos].refresh(value);
 }
 
 const ws = new WebSocket("ws://localhost:8080");
@@ -116,6 +132,46 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 });
 
+document.getElementById('setup_environment').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        const robot_count = Number(event.target.value);
+        if (Number.isInteger(robot_count) && robot_count > 0) {
+            console.log('Robot count submitted:', robot_count);
+            // Clear previous robots, gauges and plates
+            const robots = document.getElementById("robots");
+            const gauges = document.getElementById("gauges");
+            const plates = document.getElementById("plates");
+            robots.innerHTML = `<h2 class="open-sans-myfont">Robots</h2>`;
+            gauges.innerHTML = `<h2 class="open-sans-myfont">Robots Overall Time</h2>`;
+            plates.innerHTML = `<h2 class="open-sans-myfont">Conveyor</h2>`;
+            for (let position = 1; position <= robot_count; position++) {
+                // Create and append robot info element
+                const robot = create_robot_info_element(position);
+                robots.appendChild(robot);
+                // Create and append gauge info element
+                const gauge = create_gauge(position);
+                gauges.appendChild(gauge);
+                gauges[position] = new JustGage({
+                    id: `gauge-${position}`,
+                    value: 0,
+                    min: 0,
+                    max: 100,
+                    label: "units"
+                });
+                // Create and append plate info element
+                const plate = create_plate_info_element(position, `Pos ${position}`);
+                plates.appendChild(plate);
+            }
+            // Create and append plate info element
+            const plate = create_plate_info_element(0, `OUTPUT`);
+            plates.appendChild(plate);
+        } else {
+            alert('Please enter a positive integer.');
+        }
+        event.target.value = '';
+    }
+});
+
 document.getElementById('random_order_button').onclick = call_random_order_method;
 function call_random_order_method() {
     console.log("Placing random order");
@@ -124,10 +180,10 @@ function call_random_order_method() {
 
 document.getElementById('random_order_input').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        const value = Number(event.target.value);
-        if (Number.isInteger(value) && value > 0) {
-            console.log('Input submitted:', value);
-            for (let i = 0; i < value; i++) {
+        const order_count = Number(event.target.value);
+        if (Number.isInteger(order_count) && order_count > 0) {
+            console.log('Order count submitted:', order_count);
+            for (let i = 0; i < order_count; i++) {
                 ws.send("PlaceRandomOrder");
             }
         } else {
