ce75a3df8c41737b995524a3da497324561c9d93
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2025-09-16 02:24:47 +0200

    Fix element by id request

--- frontend.js
+++ frontend.js
@@ -123,14 +123,18 @@ function update_kitchen_info_element(data) {
         }
     };
     const set_remote_attributes_if_exists = (type, attribute_name, new_value) => {
-        const el = document.getElementById(`${type}-${attribute_name}`);
+        let el = null;
+        if (attribute_name === Kitchen.ASSIGNED_ORDERS || attribute_name === Kitchen.DROPPED_ORDERS) {
+            el = document.getElementById(`${type}-AssignedOrders-DroppedOrders`);
+        } else {
+            el = document.getElementById(`${type}-${attribute_name}`);
+        }
         if (el && new_value !== undefined && new_value !== null && new_value !== '') {
             if (attribute_name === Kitchen.CONNECTIVITY) {
                 el.style.color = (new_value === true) ? "green" : "red";
                 el.textContent = new_value === true ? "Online" : "Offline";
             } else if (attribute_name === Kitchen.ASSIGNED_ORDERS || attribute_name === Kitchen.DROPPED_ORDERS) {
                 /* Special handling for AssignedOrders-DroppedOrders */
-                const el = document.getElementById(`${type}-AssignedOrders-DroppedOrders`);
                 const current_text = el.textContent;
                 const parts = current_text.split('/');
                 let assigned = parts[0] ? parts[0].trim() : '0';
